933cfe0941e68689118332a8db482178a27caf37
Tyler <tyler.wright5146@gmail.com>
2022-11-30 22:53:25 -0700

    even more elegance

--- 1/elegant_soln.py
+++ 1/elegant_soln.py
@@ -2,5 +2,4 @@ with open("input1.txt") as f:
     chunks = f.read().split("\n\n")
     sums = list(map(lambda a: sum(list(map(lambda b: int(b) if b != "" else 0, a.split("\n")))), chunks))
     print(max(sums))
-    sorted_sums = sorted(sums)
-    print(sum(sorted_sums[-3:]))
+    print(sum(sorted(sums)[-3:]))
