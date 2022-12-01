c99be641e4a7fc942461802d1c58b00bd0631bcf
Tyler <tyler.wright5146@gmail.com>
2022-11-30 22:58:07 -0700

    max elegance

--- 1/elegant_soln.py
+++ 1/elegant_soln.py
@@ -1,4 +1,3 @@
 with open("input1.txt") as f:
     sums = list(map(lambda a: sum(list(map(lambda b: int(b) if b != "" else 0, a.split("\n")))), f.read().split("\n\n")))
-    print(max(sums))
-    print(sum(sorted(sums)[-3:]))
+    print(max(sums), sum(sorted(sums)[-3:]), sep="\n")
