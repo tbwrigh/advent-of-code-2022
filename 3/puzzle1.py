2ec056798bbe264d7e3def92dd4c4baa59567af4
Tyler <tyler.wright5146@gmail.com>
2022-12-02 22:23:18 -0700

    day 3

---
+++ 3/puzzle1.py
@@ -0,0 +1,24 @@
+total = 0
+with open("input.txt") as f:
+    t = f.readlines()
+
+    for line in t:
+        line = line.replace("\n", "")
+        ll = len(line)
+        sl = int(ll/2)
+        f, s = list(line[:sl]), list(line[sl:])
+        c = ""
+        c_org = c
+        for ch in f:
+            if ch in s:
+                c = ch
+                c_org = c
+        if c == c.upper():
+            c = c.lower()
+            total -= 6
+        else:
+            c = c.upper()
+        print(c, c_org, ord(c) - ord("A") + 1)
+        total += ord(c) - ord("A") + 1
+
+print(total)
---
+++ 3/puzzle2.py
@@ -0,0 +1,30 @@
+total = 0
+with open("input.txt") as f:
+    t = f.readlines()
+
+    groups = []
+
+    group = []
+
+    for i in range(len(t)):
+        group.append(t[i].replace("\n", ""))
+        if (i+1) % 3 == 0:
+            groups.append(group)
+            group = []
+
+    for group in groups:
+        c = ""
+        c_org = c
+        for ch in group[0]:
+            if ch in group[1] and ch in group[2]:
+                c = ch
+                c_org = c
+        if c == c.upper():
+            c = c.lower()
+            total -= 6
+        else:
+            c = c.upper()
+        print(c, c_org, ord(c) - ord("A") + 1)
+        total += ord(c) - ord("A") + 1
+
+print(total)
