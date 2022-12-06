8165e3656621d0b718f608593ad5203ab4688fa4
Tyler <tyler.wright5146@gmail.com>
2022-12-05 22:09:42 -0700

    day 6

---
+++ 6/puzzle1.py
@@ -0,0 +1,22 @@
+def all_uniq(s):
+    for i in range(len(s)):
+        for j in range(i+1, len(s)):
+            if s[i] == s[j]:
+                return False
+    return True
+
+with open("input.txt") as f:
+    text = f.read()
+
+    # print(text, len(text))
+
+    buffer = text[:4]
+
+    if all_uniq(buffer):
+        print("4")
+
+    for i in range(4, len(text)):
+        buffer = buffer[1:] + text[i]
+        if all_uniq(buffer):
+            print(i+1)
+            break
